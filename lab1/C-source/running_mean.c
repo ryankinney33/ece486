73895d7d1c1d0cdbb9c902530982de8702447ddb
Ryan Kinney <ryan.kinney@maine.edu>
2021-02-19 10:56:20 -0500

    Optimize calc_running_mean()

--- lab1/C-source/running_mean.c
+++ lab1/C-source/running_mean.c
@@ -1,14 +1,14 @@
 #include <stdlib.h>
 #include "running_mean.h"
 
-// shifts the elements of an array left n times, padding with 0
-void array_left_shift(float* a, int size, int n){
-	for(int i = 0; i < n ; ++i){
-		for(int j = 0; j < size-1; ++j){
-			a[j] = a[j+1];
-		}
-		a[size-1] = 0;
+// shifts the elements of an array left once and inserts val at the end
+void shift_insert(float* arr, int size, float val){
+	// shifts the elements left by 1
+	for(int i = 0; i < size-1; ++i){
+		arr[i] = arr[i+1];
 	}
+	// insert the new value in the open spot at the end
+	arr[size-1] = val;
 }
 
 struct run_mean init_running_mean(int M, int blocksize){
@@ -24,41 +24,18 @@ float* calc_running_mean(float* x, struct run_mean* s){
 	float* y = (float*)calloc(s->blocksize, sizeof(float));
 
 	// iterate over the input samples
-	for(int i = 0; i < s->blocksize; ++i){
-		// get the lower_bound for samples to add from current input
-		int lower_bound = i-s->M+1;
-		lower_bound = (lower_bound < 0)?0:lower_bound;
-
-		// find the sum of the current input values from lower_bound to i
-		for(; lower_bound <= i; ++lower_bound){
-			y[i] += x[lower_bound];
-		}
-
-		// add the sum from the previous samples
-		for(int j = i; j <= s->M-1; ++j){
+	for(int i=0; i < s->blocksize; ++i){
+		// get the sum of s->lastM
+		for(int j=0; j < s->M-1; ++j){
 			y[i] += s->lastM[j];
 		}
-		// convert the sum into the average
-		y[i] /= (float)s->M;
-    }
 
-	// shift s->lastM s->blocksize times to the left if needed
-	if(s->M > s->blocksize){
-		array_left_shift(s->lastM,s->M, s->blocksize);
+		// add the current input and find the average of the sum
+		y[i] = (y[i]+x[i])/(float)s->M;
+
+		// finally, save the current input
+		shift_insert(s->lastM, s->M-1, x[i]);
 	}
-
-	// replace the end of s->lastM with the new samples
-	// get the lowerbounds for both arrays
-	int Mlowbound = s->M-s->blocksize;
-	Mlowbound = (Mlowbound < 0)? 0:Mlowbound;
-
-	int Xlowbound = s->blocksize-(s->M-1);
-	Xlowbound = (Xlowbound < 0)? 0:Xlowbound;
-
-	for(int i = 0; (i+Mlowbound) < s->M-1 && (i+Xlowbound) < s->blocksize; ++i){
-		s->lastM[i+Mlowbound] = x[i+Xlowbound];
-	}
-
 	return y;
 }
 
