4461ecfe2ef78721db4f3bbae4bb3237ee1c5794
Ryan Kinney <ryan.kinney@maine.edu>
2021-03-14 12:25:13 -0400

    Clean up FIR testing script

--- lab2/FIR/Tests.m
+++ lab2/FIR/Tests.m
@@ -12,59 +12,45 @@
 % This test is used to verify the validity of the C program
 
 % first, create the data
-blocksize1 = 10;
-M1 = 5;
-b1 = single([1,zeros(1,M1-1)]);
-x1 = single(0:blocksize1-1);
+blocksize = 10;
+M = 5;
+b = single([1,zeros(1,M-1)]);
+x = single(0:blocksize-1);
 
 % get the output from the filter
-y1 = filter(b1,1,x1);
+y = filter(b,1,x);
 
 % print output
 fprintf('Test 1: h[n] = unit impulse\n');
 fprintf(' Discrete convolution results: h*x:\n');
 
-fprintf('  x = {') % x1
-for i = x1(1:end-1)
-    fprintf('%f, ',i)
-end
-fprintf('%f}\n',x1(end))
-
-fprintf('  y = {') % y1
-for i = y1(1:end-1)
-    fprintf('%f, ',i)
-end
-fprintf('%f}\n\n',y1(end))
+fprintf('  x = {'),print_array(x),fprintf('}\n')
+fprintf('  y = {'),print_array(y),fprintf('}\n\n')
 
 %% Test 2
 
 % h[n] is 0 before n = 0, 1 for n = 0,1,2, and 0 afterwards
 % x[n] = u[n], the unit step function
 
-blocksize2 = 10;
-M2 = 10;
-b2 = single([ones(1,3),zeros(1,M2-3)]);
-x2 = ones(1,blocksize2);
+blocksize = 10;
+M = 10;
+b = single([ones(1,3),zeros(1,M-3)]);
+x = ones(1,blocksize);
 
 % calculate the output
-y2 = filter(b2,1,x2);
+y = filter(b,1,x);
 
 % print results
 fprintf('Test 2: x[n] = u[n]\n');
-fprintf('h[n] = {'); % b2
-for i = b2(1:end-1)
-    fprintf('%f, ',i)
-end
-fprintf('%f}\n',b2(end));
+fprintf('h[n] = {'),print_array(b),fprintf('}\n')
 fprintf(' Discrete convolution results: h*x:\n');
-fprintf('  x = {') % x2
-for i = x2(1:end-1)
-    fprintf('%f, ',i)
-end
-fprintf('%f}\n',x2(end))
+fprintf('  x = {'),print_array(x),fprintf('}\n')
+fprintf('  y = {'),print_array(y),fprintf('}\n\n')
 
-fprintf('  y = {') % y2
-for i = y2(1:end-1)
+%% Local function for printing the output
+function print_array(array)
+for i = array(1:end-1)
     fprintf('%f, ',i)
 end
-fprintf('%f}\n\n',y2(end))
+fprintf('%f',array(end))
+end
